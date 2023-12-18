5dfdbc4491bed32a324567440e9cb8e90dfa1f0a
eragon shapira10 <eragon.shapira10@gmail.com>
2023-12-18 09:50:20 +0700

    update version tag

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yanuarultfah/refresh_go_moudule_app.git
+
+go 1.18
+
+require github.com/yanuarultfah/refresh_golang_module v1.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	refreshgolangmodule "github.com/yanuarultfah/refresh_golang_module"
+)
+
+func main() {
+	fmt.Println(refreshgolangmodule.SayHello())
+}
